7feda2f9c3721d4af9231048491186933abbd02f
WangBaocong <wangbaocong@bytedance.com>
2023-05-05 14:34:02 +0800

    api test

--- tests/test_project_list.py
+++ tests/test_project_list.py
@@ -1,9 +1,14 @@
+import logging
 import time
 
 import requests
 
+import urllib3
+
+
 class TestProjecList(object):
     def test_project_list(self):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         host = "https://xfcc-boe.bytedance.net"
         path = "/f100/api/nova_crm/project_list"
         params = {"page_no": 1,
@@ -11,8 +16,10 @@ class TestProjecList(object):
                   "timestamp": 1621495086298
                   }
         headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-                   "Cookie": "MONITOR_WEB_ID=6ea52aff-9e09-441e-a4fb-ae0f534e6c1d; _ga=GA1.2.613940271.1611455300; byteio_version=new; n_mh=unQRywF5kC1OJB-dfADhYqPD58vyw3bm8vWM29WII6c; sid_guard=dd596a092a36b21385263632b22863ca%7C1614599205%7C5183999%7CFri%2C+30-Apr-2021+11%3A46%3A44+GMT; gftoken=ZGQ1OTZhMDkyYXwxNjE0NTk5MzQ2Mzl8fDAGBgYGBgY; local=zh-CN; timezone=%28GMT%2B8%3A00%29%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4%26-%26%E4%B8%8A%E6%B5%B7%7C%28GMT%2B8%3A00%29China%26Standard%26Time%26-%26Shanghai%7C%28GMT%2B8%3A00%29%E4%B8%AD%E5%9B%BD%E6%A8%99%E6%BA%96%E6%99%82%26-%26%E4%B8%8A%E6%B5%B7%7C480%7CAsia%2FShanghai; googtrans=/zh-CN/zh-CN; gfsitesid=ZGQ1OTZhMDkyYXwxNjE0NTk5MzQ2Mzl8fDE1MzA5Mzk3Mzg5NTU4NjkHBwcHBwcH; admin-csrf-token=Q4KZ0pbhsWp5+0MRfIORjuwhglH81EA7ZZ3muf9yT/oqUhU2SMkmb3PYjnNWO5DK0cebV7sIdvqLRxx1KCYzGaEDxZf7W4rSnfDiS/RDxhzlvvhNqb1+YG8hcc8lWuVYNhsl0w==; f100=MTYyMDkwMDEyNXxEdi1CQkFFQ180SUFBUkFCRUFBQV80dl9nZ0FDQm5OMGNtbHVad3dHQUFSMWMyVnlEU3B6YzI4dVZYTmxja2x1Wm1fX2d3TUJBUWhWYzJWeVNXNW1id0hfaEFBQkJ3RUZSVzFoYVd3QkRBQUJCa1Z0WVdsc2N3SF9oZ0FCRFVWdGNHeHZlV1ZsU1dSVGRISUJEQUFCQWtsRUFRUUFBUVJPWVcxbEFRd0FBUWhWYzJWeWJtRnRaUUVNQUFFS1JXMXdiRzk1WldWSlpBRUVBQUFBRnYtRkFnRUJDRnRkYzNSeWFXNW5BZi1HQUFFTUFBRF96Xy1FWUFFWmQyRnVaMkpoYjJOdmJtZEFZbmwwWldSaGJtTmxMbU52YlFFQkdYZGhibWRpWVc5amIyNW5RR0o1ZEdWa1lXNWpaUzVqYjIwQkJ6WTVNakE1TURVQl9RRnNEQUVMZDJGdVoySmhiMk52Ym1jQkMzZGhibWRpWVc5amIyNW5BQVp6ZEhKcGJtY01Cd0FGZEc5clpXNE5LbTloZFhSb01pNVViMnRsYnYtSEF3RUJCVlJ2YTJWdUFmLUlBQUVFQVF0QlkyTmxjM05VYjJ0bGJnRU1BQUVKVkc5clpXNVVlWEJsQVF3QUFReFNaV1p5WlhOb1ZHOXJaVzRCREFBQkJrVjRjR2x5ZVFIX2lnQUFBQkRfaVFVQkFRUlVhVzFsQWYtS0FBQUFjZi1JYmdFb05qYzJaREk0WVRFMU5UQTBOMlk0WkdKbE9HWTFNalU0Tm1aa1ptWmxNRFkzWW1RNU1qVXdOd0VHUW1WaGNtVnlBU2czWXpjNE56TXdORFJoWm1ZMk9UTmlOR1F5T1RZeFl6SmtabUZpTXpnek9XUm1OMk01TURrekFROEJBQUFBRHRoV2ZSd0lvT1dzQWVBQXxsSVRCX0Q_BR5-zWGbPaEvgF-CEBwg_HQdsPW9BdikhQ==; tt_webid=6962063442959123999; MONITOR_WEB_ID=04285969-e1a3-47c0-9c3f-9fac2b832d52; _gid=GA1.2.1723361100.1621494621"
+                   "Cookie": "sid_guard=4c6fbbb81d98a464da6d7a17044e544a%7C1667793917%7C5183999%7CFri%2C+06-Jan-2023+04%3A05%3A16+GMT; n_mh_xfl_xfcc_boe=paF0HsdwvRjKYOyAykD2gPmGv4Be_51wk5spW7PGqaM; sid_guard_xfl_xfcc_boe=3f928ad0f391c530960f0127a301ee07%7C1679643289%7C5184000%7CTue%2C+23-May-2023+07%3A34%3A49+GMT; uid_tt_xfl_xfcc_boe=a0ba64be6c6d05483f2866e5ded93f26; uid_tt_ss_xfl_xfcc_boe=a0ba64be6c6d05483f2866e5ded93f26; sid_tt_xfl_xfcc_boe=3f928ad0f391c530960f0127a301ee07; sessionid_xfl_xfcc_boe=3f928ad0f391c530960f0127a301ee07; sessionid_ss_xfl_xfcc_boe=3f928ad0f391c530960f0127a301ee07; sid_ucp_v1_xfl_xfcc_boe=1.0.0-KGIwYTU0ZWI2ZDVlYTJjMWMyOGM5NTdmOWVmZTg5MjM2ZjIwODY5ZWQKFwjOpuGTvo2MAxCZpfWgBhiTOzgCQPEHEH8aA2JvZSIgM2Y5MjhhZDBmMzkxYzUzMDk2MGYwMTI3YTMwMWVlMDc; ssid_ucp_v1_xfl_xfcc_boe=1.0.0-KGIwYTU0ZWI2ZDVlYTJjMWMyOGM5NTdmOWVmZTg5MjM2ZjIwODY5ZWQKFwjOpuGTvo2MAxCZpfWgBhiTOzgCQPEHEH8aA2JvZSIgM2Y5MjhhZDBmMzkxYzUzMDk2MGYwMTI3YTMwMWVlMDc; local=zh-CN; timezone=%28GMT%2B8%3A00%29%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4%26-%26%E4%B8%8A%E6%B5%B7%7C%28GMT%2B8%3A00%29China%26Standard%26Time%26-%26Shanghai%7C%28GMT%2B8%3A00%29%E4%B8%AD%E5%9B%BD%E6%A8%99%E6%BA%96%E6%99%82%26-%26%E4%B8%8A%E6%B5%B7%7C480%7CAsia%2FShanghai; s_v_web_id=verify_lgknfs34_CNRRy72M_Q57Z_4iPT_BUFT_07CUiHcGwMhH; X-Risk-Browser-Id=1e6ee1672fd0d489360c3fff707fafee939b4d17711cf03c67a90e1137a7ff79; _tea_utm_cache_7571=undefined; gfsitesid=MTk2NjEwMjQyN3wxNjgzMTg1NzA1NTl8fDAGBgYGBgY; gftoken=MTk2NjEwMjQyN3wxNjgzMTg1NzA1NTl8fDAGBgYGBgY"
         }
         r = requests.request("GET", url=host+path, params=params, headers=headers, verify=False)
         response = r.json()
+        logging.info('11111')
+        print(response)
         assert response["message"] == "success"
